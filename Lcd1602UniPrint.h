3a46d65bee9a980f8aebcda84ecfe1b48e548905
Tyrn <alexey1911@gmail.com>
2025-04-05 19:21:32 +0300

    First commit formatted

--- Lcd1602UniPrint.h
+++ Lcd1602UniPrint.h
@@ -7,9 +7,10 @@
 
 class Lcd1602UniPrint {
 public:
-  Lcd1602UniPrint(uint8_t lcd_Addr = 0x27, uint8_t lcd_cols = 16, uint8_t lcd_rows = 2)
-    : d(lcd_Addr, lcd_cols, lcd_rows) {}
-  
+  Lcd1602UniPrint(uint8_t lcd_Addr = 0x27, uint8_t lcd_cols = 16,
+                  uint8_t lcd_rows = 2)
+      : d(lcd_Addr, lcd_cols, lcd_rows) {}
+
   // The length of a Unicode string in characters.
   static int len(const char *s);
 
@@ -26,6 +27,7 @@ public:
   void print_dec_ul(const char *label, int value);
 
   LCD_1602_RUS d;
+
 private:
 };
 
